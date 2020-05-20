d2806387d369111d82a8ed6e41fa910a69390e7f
Allison Mackey <a.mackey693@gmail.com>
2020-05-20 10:36:27 -0700

    add logic to pass test on spec #== method, test passes

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 class Book 
-  att_accessor :name
+  attr_accessor :name, :id
+
+  def initialize(attributes) 
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def ==(book_to_compare)
+    self.name == book_to_compare.name
+  end
+
+  def self.all
+    # returned_books =
+  end
 end 
